9a4e69266ae37114bee5e475de36b0cd7b14e092
AhmadMatar4191 <ahmma483@student.liu.se>
2026-02-07 14:11:31 +0100

    Fix import path for App.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/app.css";
+import "./styles/App.css";
 import "./styles/map.css";
 import "./styles/sheet.css";
 
